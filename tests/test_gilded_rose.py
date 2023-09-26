3e0e51519f09431323ba84588af8c7f20d8e8075
Oliver Schwartz <olsch88@googlemail.com>
2023-09-26 13:30:40 +0200

    added tests for default behaviour

--- tests/test_gilded_rose.py
+++ tests/test_gilded_rose.py
@@ -1,6 +1,6 @@
 import pytest
 from hypothesis import given
-from hypothesis.strategies import text
+from hypothesis.strategies import text, integers
 
 
 from ..gilded_rose import Item, GildedRose
@@ -14,12 +14,25 @@ def test_no_name_change(sample_name):
     assert sample_name == items[0].name
 
 
-def test_quality_decreases_default():
-    items = [Item("foo", sell_in=5, quality=5)]
+@given(start_quality=integers(min_value=1, max_value=50))
+def test_quality_decreases_default(start_quality):
+    items = [Item("foo", sell_in=5, quality=start_quality)]
     gilded_rose = GildedRose(items)
     gilded_rose.update_quality()
-    assert items[0].quality == 4
+    assert items[0].quality == max(0, start_quality - 1)
 
 
-# if __name__ == "__main__":
-#     unittest.main()
+@given(start_quality=integers(min_value=1, max_value=50))
+def test_quality_decreases_after_date(start_quality):
+    items = [Item("foo", sell_in=-1, quality=start_quality)]
+    gilded_rose = GildedRose(items)
+    gilded_rose.update_quality()
+    assert items[0].quality == max(0, start_quality - 2)
+
+
+@given(start_sell_in=integers(min_value=1))
+def test_sell_in_decreases_default(start_sell_in):
+    items = [Item("foo", sell_in=start_sell_in, quality=5)]
+    gilded_rose = GildedRose(items)
+    gilded_rose.update_quality()
+    assert items[0].sell_in == start_sell_in - 1
