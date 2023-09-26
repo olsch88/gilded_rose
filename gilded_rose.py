91dd89401bd5cee1730e833a1ec62a3d0bc7367d
Oliver Schwartz <olsch88@googlemail.com>
2023-09-26 14:04:53 +0200

    moved Item class to top of file created placeholder functions

--- gilded_rose.py
+++ gilded_rose.py
@@ -9,6 +9,24 @@ class Articles(StrEnum):
     SULFURAS = "Sulfuras, Hand of Ragnaros"
 
 
+class Item:
+    def __init__(self, name, sell_in, quality):
+        self.name = name
+        self.sell_in = sell_in
+        self.quality = quality
+
+    def __repr__(self):
+        return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+
+
+def increase_quality(item: Item) -> None:
+    pass
+
+
+def decrease_quality(item: Item) -> None:
+    pass
+
+
 class GildedRose(object):
     def __init__(self, items):
         self.items = items
@@ -42,13 +60,3 @@ class GildedRose(object):
                 else:
                     if item.quality < 50:
                         item.quality = item.quality + 1
-
-
-class Item:
-    def __init__(self, name, sell_in, quality):
-        self.name = name
-        self.sell_in = sell_in
-        self.quality = quality
-
-    def __repr__(self):
-        return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
