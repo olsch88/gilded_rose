7cbc0c4d06d1f033630b454e6ed846301ae0fd69
Oliver Schwartz <olsch88@googlemail.com>
2023-09-26 14:00:52 +0200

    Replaced strings with enum

--- gilded_rose.py
+++ gilded_rose.py
@@ -15,30 +15,27 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if (
-                item.name != Articles.AGED_BRIE
-                and item.name != "Backstage passes to a TAFKAL80ETC concert"
-            ):
+            if item.name != Articles.AGED_BRIE and item.name != Articles.BACKSTAGE_PASS:
                 if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
+                    if item.name != Articles.SULFURAS:
                         item.quality = item.quality - 1
             else:
                 if item.quality < 50:
                     item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                    if item.name == Articles.BACKSTAGE_PASS:
                         if item.sell_in < 11:
                             if item.quality < 50:
                                 item.quality = item.quality + 1
                         if item.sell_in < 6:
                             if item.quality < 50:
                                 item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
+            if item.name != Articles.SULFURAS:
                 item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
+                if item.name != Articles.AGED_BRIE:
+                    if item.name != Articles.BACKSTAGE_PASS:
                         if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
+                            if item.name != Articles.SULFURAS:
                                 item.quality = item.quality - 1
                     else:
                         item.quality = item.quality - item.quality
